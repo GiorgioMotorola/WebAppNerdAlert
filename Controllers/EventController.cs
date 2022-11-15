396f17446403ad61a45b48923e33465123af3792
Mike Weatherford <mikesweatherford@gmail.com>
2022-11-14 19:19:06 -0500

    added views

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppNerdAlert.Data;
+using WebAppNerdAlert.Models;
 
 namespace WebAppNerdAlert.Controllers
 {
     public class EventController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public EventController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Event> events = _context.Events.ToList();
+            return View(events);
         }
     }
 }
--- Controllers/HobbyController.cs
+++ Controllers/HobbyController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppNerdAlert.Data;
+using WebAppNerdAlert.Models;
 
 namespace WebAppNerdAlert.Controllers
 {
     public class HobbyController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HobbyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Hobby> hobbies = _context.Hobbies.ToList();
+            return View(hobbies);
         }
     }
 }
