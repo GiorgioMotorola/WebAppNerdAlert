89faea201106eacb4120af42364ba8f10f5f964f
Mike Weatherford <mikesweatherford@gmail.com>
2022-11-23 19:07:48 -0500

    added a login page that validates users or returns an error that the email or password is wrong

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAppNerdAlert.Data;
+using WebAppNerdAlert.Models;
+using WebAppNerdAlert.ViewModels;
+
+namespace WebAppNerdAlert.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _signInManager = signInManager;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel LoginViewModel)
+        {
+            if (!ModelState.IsValid) return View(LoginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(LoginViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, LoginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, LoginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Hobby");
+                    }
+                }
+                TempData["Error"] = "Wrong Email or Password";
+                return View(LoginViewModel);
+            }
+            TempData["Error"] = "Wrong Email or Password";
+            return View(LoginViewModel);
+        }
+
+
+    }
+
+
+}
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppNerdAlert.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email is Required")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
